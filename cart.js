5895e6d871a83e00df05898e162e26bf193ca8cc
Олег Шевченко <shevchenko.code@yandex.ru>
2022-08-25 12:58:21 +0300

    finish device html css & a lil bit js

--- cart.js
+++ cart.js
@@ -1,7 +1,30 @@
-const cart = document.querySelector('.cart-list'); 
+let deleteItemFromCartButtons = document.getElementsByClassName('item-delete');
 
-cart.addEventListener('click', (e) => {
-  if (e.target.classList.contains('item-delete')) {
-    e.target.closest('.cart-item').classList.add('deleted');
+for (let i = 0; i < deleteItemFromCartButtons.length; i++) {
+  let button = deleteItemFromCartButtons[i];
+  button.addEventListener('click', removeCartItem);
+}
+
+function removeCartItem(event) {
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateCartTotal();
+}
+
+function updateCartTotal() {
+  let cartItemContainers = document.getElementsByClassName('cart-item')[0]
+  let cartItems = cartItemContainers.getElementsByClassName('cart-item-link')
+  let total = 0
+  for (let i = 0; i < cartItems.length; i++) {
+    let cartItem = cartItems[i]
+    let priceElement = cartItem.getElementsByClassName('cart-item-price')[0]
+    let price = parseFloat(priceElement.innerText)
+    total = total + price
   }
-});
+
+  if (total <= 0 || isNaN(total)) {
+    total = 0;
+  }
+
+  document.getElementsByClassName('cart-sum-price')[0].innerText = total
+}
